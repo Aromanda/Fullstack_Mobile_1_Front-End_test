22f0eee9799c2d5123b2ecafabb9cad4b90f6a52
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-08-14 08:44:57 -0400

    restaurantsmenus page organisée

--- components/screen/restaurantsmenus.js
+++ components/screen/restaurantsmenus.js
@@ -2,15 +2,42 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, FlatList, Image } from 'react-native';
 import CustomNavbar from '../../components/shared/header';
 import FooterNavbar from '../../components/shared/footer';
+import { useNavigation } from '@react-navigation/native';
 
 const RestaurantsMenus = ({ route }) => {
   const { restaurant_id } = route.params;
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [restaurant, setRestaurant] = useState(null);
+  const [quantities, setQuantities] = useState({});
+  const [createOrderEnabled, setCreateOrderEnabled] = useState(false);
+  const navigation = useNavigation(); 
+
+  const navigateToRestaurants = () => {
+    navigation.navigate('Restaurants'); // Navigate back to the Restaurants component
+  };
+
+  const increment = (itemId) => {
+    if (quantities[itemId] === undefined) {
+      setQuantities({ ...quantities, [itemId]: 1 });
+    } else {
+      setQuantities({ ...quantities, [itemId]: quantities[itemId] + 1 });
+    }
+  };
+
+  const decrement = (itemId) => {
+    if (quantities[itemId] === undefined || quantities[itemId] <= 0) {
+      return;
+    }
+    setQuantities({ ...quantities, [itemId]: quantities[itemId] - 1 });
+  };
 
   useEffect(() => {
-    // Fetch restaurant details
+    const isCreateOrderEnabled = Object.values(quantities).some((quantity) => quantity > 0);
+    setCreateOrderEnabled(isCreateOrderEnabled);
+  }, [quantities]);
+
+  useEffect(() => {
     const url = `${process.env.EXPO_PUBLIC_NGROK_URL}/api/restaurants`;
     fetch(url)
       .then((response) => response.json())
@@ -22,130 +49,218 @@ const RestaurantsMenus = ({ route }) => {
         console.error('Error fetching restaurant details:', error);
       });
 
-    // Fetch products for the restaurant
     const productsUrl = `${process.env.EXPO_PUBLIC_NGROK_URL}/api/products?restaurant=${restaurant_id.id}`;
     fetch(productsUrl)
       .then((response) => response.json())
       .then((data) => {
         setProducts(data);
-        setLoading(false); // Set loading to false once products are fetched
+        setLoading(false);
       })
       .catch((error) => {
         console.error('Error fetching products:', error);
-        setLoading(false); // Set loading to false even if there's an error
+        setLoading(false);
       });
+
+    setQuantities({}); // Reset quantities when changing restaurant
   }, [restaurant_id]);
 
+  const renderStars = (rating) => {
+    const filledStars = '★'.repeat(Math.floor(rating));
+    const emptyStars = '☆'.repeat(5 - Math.floor(rating));
+  
+    const stars = [];
+    for (let i = 0; i < Math.floor(rating); i++) {
+      stars.push(<Text key={`filled-${i}`} style={styles.star}>★</Text>);
+    }
+    for (let i = 0; i < 5 - Math.floor(rating); i++) {
+      stars.push(<Text key={`empty-${i}`} style={styles.star}>☆</Text>);
+    }
+  
+    return <View style={styles.ratingContainer}>{stars}</View>;
+  };  
 
-  return (
-    <View style={{ flex: 1 }}>
-      <CustomNavbar />
-  
-      <View style={styles.container}>
-        <Text style={styles.headerText}>RESTAURANT MENU</Text>
-  
-        <View style={styles.restaurantInfo}>
-          <View style={styles.restaurantInfoText}>
-            {/* <Text style={styles.restaurantName}>{restaurant_id.name}</Text> */}
-            <Text style={styles.restaurantName}>{restaurant?.name}</Text>
-            <View style={styles.menuItem}>
-              <Text style={styles.menuItemLabel}>Price:</Text>
-              <Text style={styles.menuItemValue}>{"$".repeat(restaurant?.price_range)}</Text>
-            </View>
-            <View style={styles.menuItem}>
-              <Text style={styles.menuItemLabel}>Rating:</Text>
-              <Text style={styles.menuItemValue}>{"★".repeat(restaurant?.rating)}</Text>
-            </View>
-          </View>
-          <TouchableOpacity style={styles.createOrderButton}>
-            <Text style={styles.createOrderButtonText}>Create Order</Text>
-          </TouchableOpacity>
-        </View>
-        
-        {/* Display the list of products here */}
-        <FlatList
-  data={products}
-  renderItem={({ item }) => (
-    <View style={styles.productItem}>
-      <Image
-        source={require('../../assets/Images/RestaurantMenu.jpg')}
-        style={styles.productImage}
-      />
-      <View style={styles.productInfo}>
-        <Text style={styles.productName}>{item.name}</Text>
-        <Text style={styles.productPrice}>${item.price}</Text>
+  const defaultImage = require('../../assets/Images/RestaurantMenu.jpg');
+
+  const renderProduct = ({ item }) => (
+    <View style={styles.card}>
+      <Image source={defaultImage} style={styles.image} />
+      <View style={styles.textContainer}>
+        <Text style={styles.productTitle}>{item.name}</Text>
+        <Text style={styles.price}>${item.price}</Text>
+      </View>
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity style={styles.button} onPress={() => decrement(item.id)}>
+          <Text style={styles.buttonText}>-</Text>
+        </TouchableOpacity>
+        <Text>{quantities[item.id] || 0}</Text>
+        <TouchableOpacity style={styles.button} onPress={() => increment(item.id)}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
       </View>
     </View>
-  )}
-  keyExtractor={(item) => item.id.toString()}
-/>
-    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+      <CustomNavbar />
+      <View style={styles.contentContainer}>
+        <Text style={styles.menuTitle}>RESTAURANTS MENU</Text>
+        {restaurant && (
+          <View style={styles.restaurantInfoContainer}>
+            {/* Restaurant information */}
+            <View style={styles.restaurantInfo}>
+              <Text>{restaurant.name}</Text>
+              <Text style={styles.info}>Price: ${restaurant.price_range}</Text>
+              <View style={styles.ratingContainer}>
+                <Text>Rating: </Text>
+                {renderStars(restaurant.rating)} 
+              </View>
+            </View>
+            <TouchableOpacity
+              style={[styles.createOrderButton, !createOrderEnabled && styles.disabledButton]}
+              disabled={!createOrderEnabled}
+              onPress={navigateToRestaurants}
+            >
+              {/* Create Order button */}
+              <Text style={styles.createOrderButtonText}>Create Order</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+        {loading ? (
+          <Text>Loading...</Text>
+        ) : (
+          <FlatList
+            data={products}
+            renderItem={renderProduct}
+            keyExtractor={(item) => item.id.toString()}
+            style={styles.productList} // Add a style to control FlatList width
+          />
+        )}
+      </View>
       <FooterNavbar />
     </View>
   );  
-};
+};  
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  headerText: {
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    width: '100%', // Occupy the full available width
+    paddingTop: 10,
+  },
+  restaurantInfo: {
+    flex: 1, // Take available space
+  },
+  restaurantInfoContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between', // Align items in a row with space between
+    alignItems: 'center', // Align items vertically
+    marginBottom: 10,
+    paddingHorizontal: 20, // Add horizontal padding for spacing
+  },
+  menuTitle: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
   },
-  restaurantInfo: {
-    flexDirection: 'row', // Align items horizontally
-    justifyContent: 'space-between', // Space between items
-    alignItems: 'center', // Vertically align items
-    width: '100%', // Occupy full width
-    paddingHorizontal: 20, // Add some horizontal padding
-    marginBottom: 20, // Add some margin
-  },
-  restaurantInfoText: {
-    // Add any additional styles for the restaurant info text container here
-  },
-  restaurantName: {
+  restaurantTitle: {
     fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 10,
   },
-  menuItem: {
+  info: {
+    marginBottom: 10,
+  },
+  ratingContainer: {
     flexDirection: 'row',
     alignItems: 'center',
+  },
+  card: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  image: {
+    width: 50,
+    height: 50,
+    resizeMode: 'cover',
+    marginRight: 10,
+  },
+  textContainer: {
+    flex: 1,
+    marginRight: 10,
+  },
+  productTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
     marginBottom: 5,
   },
-  menuItemLabel: {
-    fontWeight: 'bold',
-    marginRight: 5,
+  price: {
+    fontSize: 14,
+    color: '#888',
   },
-  menuItemValue: {},
-  createOrderContainer: {
+  buttonsContainer: {
+    flexDirection: 'row',
     alignItems: 'center',
-    marginTop: 20,
+  },
+  button: {
+    backgroundColor: '#DA583B',
+    borderRadius: 15,
+    width: 30,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
   createOrderButton: {
     backgroundColor: '#DA583B',
     borderRadius: 5,
     paddingVertical: 10,
     paddingHorizontal: 20,
+    marginTop: 20,
   },
   createOrderButtonText: {
     color: 'white',
     fontWeight: 'bold',
   },
-  ordersTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 10,
+  disabledButton: {
+    backgroundColor: '#ccc',
   },
-  productImage: {
-    width: 50,
-    height: 50,
-    resizeMode: 'cover',
-    marginRight: 10,
+  ratingContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  star: {
+    fontSize: 18, // Adjust the font size as needed
+  },
+  backButton: {
+    backgroundColor: '#DA583B',
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginTop: 10,
+    alignSelf: 'center', // Align the button to the center horizontally
+  },
+  backButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  productList: {
+    width: '100%', // Set the width of the FlatList
   },
 });
 
